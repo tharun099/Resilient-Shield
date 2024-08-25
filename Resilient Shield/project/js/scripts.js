4356b901b4c0c0b7549b28f086e45e52a5ed440b
Tharun gadupudi <128580546+tharun099@users.noreply.github.com>
2024-08-25 09:58:44 +0530

    Version 1.0

---
+++ Resilient Shield/project/js/scripts.js
@@ -0,0 +1,10 @@
+/*!
+* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
